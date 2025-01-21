2c3ea439574372d6e7a102300994e695a1c44a1a
Lori Rothermel <lori@rothermel1.com>
2025-01-21 01:55:11 -0800

    Widget designed but not yet added.

---
+++ Dex5/FetchedImage.swift
@@ -0,0 +1,18 @@
+//
+//  FetchedImage.swift
+//  Dex5
+//
+//  Created by Lori Rothermel on 1/21/25.
+//
+
+import SwiftUI
+
+struct FetchedImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FetchedImage()
+}
---
+++ Dex5/WidgetPokemon.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetPokemon.swift
+//  Dex5
+//
+//  Created by Lori Rothermel on 1/21/25.
+//
+
+import SwiftUI
+
+struct WidgetPokemon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetPokemon()
+}
